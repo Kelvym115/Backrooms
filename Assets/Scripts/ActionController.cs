5b8c781d2c90e029f974dc39971bdc6b617771d7
Kelvym115 <51379236+Kelvym115@users.noreply.github.com>
2022-10-09 01:57:55 -0300

    Finishing doors mechanics and optimizing back faces

--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class ActionController : MonoBehaviour
 {
-    public Animator door1Anim;
-    public Animator door2Anim;
+    private Animator door1Anim;
+    private Animator door2Anim;
+    private Animator door3Anim;
+    private Animator door4Anim;
 
-    public bool door1Open;
-    public bool door2Open;
+    private bool door1Open;
+    private bool door2Open;
+    private bool door3Open;
+    private bool door4Open;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,10 @@ public class ActionController : MonoBehaviour
         door1Anim = GameObject.Find("Door1").GetComponent<Animator>();
         door2Open = true;
         door2Anim = GameObject.Find("Door2").GetComponent<Animator>();
+        door3Open = true;
+        door3Anim = GameObject.Find("Door3").GetComponent<Animator>();    
+        door4Open = true;
+        door4Anim = GameObject.Find("Door4").GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +32,8 @@ public class ActionController : MonoBehaviour
     {
         door1Anim.SetBool("isOpen", door1Open);
         door2Anim.SetBool("isOpen", door2Open);
+        door3Anim.SetBool("isOpen", door3Open);
+        door4Anim.SetBool("isOpen", door4Open);
 
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
@@ -47,6 +57,20 @@ public class ActionController : MonoBehaviour
                         door2Open = false;
                     } else {
                         door2Open = true;
+                    }
+                } else if(hit.collider.gameObject.name == "Door3_Wood"){
+                    print(hit.collider.gameObject.name);
+                    if(door3Open){
+                        door3Open = false;
+                    } else {
+                        door3Open = true;
+                    }
+                } else if(hit.collider.gameObject.name == "Door4_Wood"){
+                    print(hit.collider.gameObject.name);
+                    if(door4Open){
+                        door4Open = false;
+                    } else {
+                        door4Open = true;
                     }
                 }
             }
